3c6acd64303b8f2590322bb63db58528bef973f9
patsatsia <pats.giorgi@gmail.com>
2022-11-27 22:38:46 +0100

    Alter main file
    Register a new router from 'blogs'
    
    Run 'create_all' function on start up

--- src/main.py
+++ src/main.py
@@ -1,13 +1,14 @@
 from fastapi import FastAPI
+from src.blog.router import router as blog_router
+from src.database import Base, engine
+
+Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.include_router(blog_router)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
