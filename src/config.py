b63f3f366a4bcb839757ede853535d0abc30b5b9
patsatsia <pats.giorgi@gmail.com>
2022-11-27 22:40:32 +0100

    Declare a new 'Settings' config class

---
+++ src/config.py
@@ -0,0 +1,15 @@
+import os
+
+from pydantic import BaseSettings
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = 'FastAPI Developer Guide'
+    DB_NAME: str = os.environ.get('DB_NAME')
+    DB_USER: str = os.environ.get('DB_USER')
+    DB_PASSWORD: str = os.environ.get('DB_PASSWORD')
+    DB_PORT: int = os.environ.get('DB_PORT')
+    DB_HOST: str = os.environ.get('DB_HOST')
+
+
+settings = Settings()
