63bd1d2e66bf8ad4be0966259aa398e40d256341
patsatsia <pats.giorgi@gmail.com>
2022-11-27 23:58:45 +0100

    Add a new endpoint to get a single blog item

--- src/blog/router.py
+++ src/blog/router.py
@@ -1,4 +1,5 @@
-from fastapi import APIRouter, Depends
+from uuid import UUID
+from fastapi import APIRouter, Depends, HTTPException
 from starlette import status
 from sqlalchemy.orm import Session
 
@@ -8,13 +9,13 @@ from src.database import get_db
 router = APIRouter()
 
 
-@router.get("/blogs/", response_model=schemas.BlogRetrieveList)
+@router.get("/blog/", response_model=schemas.BlogRetrieveList)
 def retrieve_all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return {"blogs": blogs}
 
 
-@router.post("/blogs/", response_model=schemas.BlogRetrieve, status_code=status.HTTP_201_CREATED)
+@router.post("/blog/", response_model=schemas.BlogRetrieve, status_code=status.HTTP_201_CREATED)
 def create_blog(blog_data: schemas.BlogCreate, db: Session = Depends(get_db)):
     blog = models.Blog(
         title=blog_data.title,
@@ -25,3 +26,11 @@ def create_blog(blog_data: schemas.BlogCreate, db: Session = Depends(get_db)):
 
     db.refresh(blog)
     return blog
+
+
+@router.get("/blog/{blog_uuid}/", response_model=schemas.BlogRetrieve)
+def retrieve_single_blog(blog_uuid: UUID, db: Session = Depends(get_db)):
+    if blog := db.query(models.Blog).get(str(blog_uuid)):
+        return blog
+
+    raise HTTPException(status_code=404, detail="Item not found")
