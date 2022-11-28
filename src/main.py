e1f43099c62a1b6ef041826dbf8a2c20fa23c719
patsatsia <pats.giorgi@gmail.com>
2022-11-28 16:52:51 +0100

    Include project title when starting up a project

--- src/main.py
+++ src/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
 from src.blog.router import router as blog_router
+from src.config import settings
 from src.database import Base, engine
 
 Base.metadata.create_all(engine)
 
-app = FastAPI()
+app = FastAPI(title=settings.APP_NAME)
 
 app.include_router(blog_router)
 
