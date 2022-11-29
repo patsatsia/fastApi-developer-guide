2d8d3573b5672c773c42d03f8ed99f084337e091
patsatsia <pats.giorgi@gmail.com>
2022-11-30 00:04:31 +0100

    Add a new path operation 'login_user'
    Minor variable name changes for 'create_user'

--- src/user/router.py
+++ src/user/router.py
@@ -3,7 +3,7 @@ from fastapi.routing import APIRouter
 from sqlalchemy.orm import Session
 from starlette import status
 
-from src.core.authentication import get_hashed_password
+from src.core.authentication import get_hashed_password, verify_password, get_jwt_tokens
 from src.database import get_db
 from src.user import schemas, crud
 
@@ -11,8 +11,8 @@ router = APIRouter()
 
 
 @router.post('/user/register/', response_model=schemas.UserOut)
-def create_user(data: schemas.UserAuth, db: Session = Depends(get_db)):
-    user = crud.retrieve_user_with_email(db=db, email=data.email)
+def create_user(user_data: schemas.UserAuth, db: Session = Depends(get_db)):
+    user = crud.retrieve_user_with_email(db=db, email=user_data.email)
 
     if user is not None:
         raise HTTPException(
@@ -21,6 +21,20 @@ def create_user(data: schemas.UserAuth, db: Session = Depends(get_db)):
         )
 
     return crud.create_user(db=db, user_data={
-        'email': data.email,
-        'password': get_hashed_password(data.password),
+        'email': user_data.email,
+        'username': user_data.username,
+        'password': get_hashed_password(user_data.password),
     })
+
+
+@router.post('/user/login/', response_model=schemas.UserTokens)
+def login_user(credentials: schemas.UserLogin, db: Session = Depends(get_db)):
+    user = crud.retrieve_user_with_email(db=db, email=credentials.email)
+
+    if not user or not verify_password(password=credentials.password, hashed_pass=user.password):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="User with given credentials does not exist"
+        )
+
+    return get_jwt_tokens(subject=user.email)
